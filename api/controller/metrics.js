bef4ef6f38d2bbacf1184ed1cf76477f065da073
vishaldandwate8411 <vishal.dandwate@aretove.com>
2021-06-30 20:50:25 +0530

    added weekly monthly metrics table

--- api/controller/metrics.js
+++ api/controller/metrics.js
@@ -25,3 +25,27 @@ export const getBaseWeeklyMetrics = async (req, res) => {
     });
   }
 };
+export const getBaseMonthlyMetrics = async (req, res) => {
+  try {
+    const forecastedMonthlyMetrics = await prisma.forecasted_monthly_metrics.findMany(
+      {
+        where: {
+          demand_forecast_run_log_id: 1,
+        },
+      }
+    );
+    const baseMonthlyMetrics = JSON.stringify(
+      forecastedMonthlyMetrics,
+      (key, value) => (typeof value === "bigint" ? value.toString() : value) // return everything else unchanged
+    );
+    res.status(200).json({
+      baseMonthlyMetrics,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "something went wrong in monthly metrics api",
+      error: `${error}`,
+    });
+  }
+};
+
---
+++ components/Metrics/MonthlyMetricsTable.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12">
+      <card card-body-classes="table-full-width">
+        <h4 slot="header" class="card-title text-bold font-weight-bold">
+          {{ tableHeading }}
+        </h4>
+        <el-table :data="metricsTableData">
+          <el-table-column
+            min-width="150"
+            sortable
+            label="Metrics Name"
+            property="metrics_name"
+          ></el-table-column>
+
+          <el-table-column
+            min-width="150"
+            sortable
+            label="Jan"
+            property="jan"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Feb"
+            property="feb"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Mar"
+            property="mar"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Apr"
+            property="apr"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="May"
+            property="may"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Jun"
+            property="jun"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Jul"
+            property="jul"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Aug"
+            property="aug"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Sep"
+            property="sep"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Oct"
+            property="oct"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Nov"
+            property="nov"
+          ></el-table-column
+          ><el-table-column
+            min-width="150"
+            sortable
+            label="Dec"
+            property="dec"
+          ></el-table-column>
+        </el-table>
+      </card>
+    </div>
+  </div>
+</template>
+<script>
+import { Table, TableColumn } from "element-ui";
+
+export default {
+  name: "dashboard",
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+  },
+  props: ["tableHeading", "metricsTableData"],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+};
+</script>
+<style></style>
