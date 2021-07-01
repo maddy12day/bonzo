4b39028ce7ccc55a41e4050df623aa551302a3dd
vishaldandwate8411 <vishal.dandwate@aretove.com>
2021-07-01 21:37:01 +0530

    added program filter with switcher

--- pages/forecast.vue
+++ pages/forecast.vue
@@ -8,20 +8,70 @@
     <!-- Filter: Revenue table (Jubin) -->
     <!-- Filter: Units table (Jubin) -->
     <!-- Filter: AUR table (Jubin) -->
-    <RegularFilters :showAplyFilterBtn="true"/>
+    <div class="col-md-12 text-right">
+      <div class="btn-group btn-group-toggle p-0 mb-2" data-toggle="buttons">
+        <label
+          v-for="(option, index) in filtersType"
+          :key="option.name"
+          class="btn btn-sm btn-primary btn-simple"
+          :id="index"
+          :class="{ active: activeFilterType == option.name }"
+        >
+          <input
+            type="radio"
+            name="options"
+            autocomplete="off"
+            checked=""
+            @click="showFilterType(option.name)"
+          />
+          <span class="d-none d-sm-block">{{ option.name }}</span>
+          <span class="d-block d-sm-none">
+            <i :class="option.icon"></i>
+          </span>
+        </label>
+      </div>
+      <RegularFilters
+        :showAplyFilterBtn="true"
+        v-if="activeFilterType == 'Regular'"
+      />
+      <ProgramFilters
+        :showAplyFilterBtn="true"
+        v-if="activeFilterType == 'Program'"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import RegularFilters from "../components/Filters/RegularFilter.vue";
+import ProgramFilters from "../components/Filters/ProgramFilter.vue";
 export default {
   name: "Forecast",
   components: {
     RegularFilters,
+    ProgramFilters,
   },
 
   data() {
-    return {};
+    return {
+      activeFilterType: "Regular",
+    };
+  },
+  methods: {
+    showFilterType(type) {
+      this.activeFilterType = type;
+    },
+  },
+  computed: {
+    filtersType() {
+      return [
+        { name: "Regular", icon: "tim-icons icon-single-02" },
+        {
+          name: "Program",
+          icon: "tim-icons icon-gift-2",
+        },
+      ];
+    },
   },
 };
 </script>
--- pages/scenarios.vue
+++ pages/scenarios.vue
@@ -1,28 +1,67 @@
 <template>
   <div>
-    <ScenarioTable
-      tableHeading="Your Scenarios"
-      :scenarioTableData="sharedScenariosList.scenarios"
-      :type="'yourScenarios'"
-      v-if="sharedScenariosList.scenarios"
-    />
-    <RegularFilters :showAplyFilterBtn="false"/>
+    <div class="col-md-12 text-right">
+      <div class="btn-group btn-group-toggle p-0 mb-2" data-toggle="buttons">
+        <label
+          v-for="(option, index) in filtersType"
+          :key="option.name"
+          class="btn btn-sm btn-primary btn-simple"
+          :id="index"
+          :class="{ active: activeFilterType == option.name }"
+        >
+          <input
+            type="radio"
+            name="options"
+            autocomplete="off"
+            checked=""
+            @click="showFilterType(option.name)"
+          />
+          <span class="d-none d-sm-block">{{ option.name }}</span>
+          <span class="d-block d-sm-none">
+            <i :class="option.icon"></i>
+          </span>
+        </label>
+      </div>
+      <RegularFilters
+        :showAplyFilterBtn="false"
+        v-if="activeFilterType == 'Regular'"
+      />
+      <ProgramFilters
+        :showAplyFilterBtn="false"
+        v-if="activeFilterType == 'Program'"
+      />
+      <ScenarioTable
+        class="mt-2"
+        tableHeading="Your Scenarios"
+        :scenarioTableData="sharedScenariosList.scenarios"
+        :type="'yourScenarios'"
+        v-if="sharedScenariosList.scenarios"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import ScenarioTable from "../components/Scenarios/ScenarioTable.vue";
 import RegularFilters from "../components/Filters/RegularFilter.vue";
+import ProgramFilters from '../components/Filters/ProgramFilter.vue';
 
 export default {
   data() {
     return {
       sharedScenariosList: [],
+      activeFilterType: "Regular",
     };
   },
   components: {
     ScenarioTable,
     RegularFilters,
+    ProgramFilters
+  },
+  methods: {
+    showFilterType(type) {
+      this.activeFilterType = type;
+    },
   },
   async mounted() {
     let allUserInfo = JSON.parse(window.localStorage.getItem("allUsersInfo"));
@@ -35,6 +74,17 @@ export default {
         progress: true,
       }
     );
+  },
+  computed: {
+    filtersType() {
+      return [
+        { name: "Regular", icon: "tim-icons icon-single-02" },
+        {
+          name: "Program",
+          icon: "tim-icons icon-gift-2",
+        },
+      ];
+    },
   },
 };
 </script>
