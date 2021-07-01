7d7b95e30a5562d3708cef9925fcbd1ad2099d94
anilmitra440 <anilm@aretove.com>
2021-07-01 20:29:01 +0530

    manually removed the conflicts in api/index.js

--- api/index.js
+++ api/index.js
@@ -5,11 +5,8 @@ import user from "./routes/user";
 import scenario from "./routes/scenario";
 import metrics from "./routes/metrics";
 import chart from "./routes/chart";
-<<<<<<< HEAD
-import cache from './routes/cache';
-=======
+import cache from "./routes/cache";
 import bonzoMl from "./routes/bonzo-ml";
->>>>>>> 615949004b892e79b1fef549ed75b5978291279d
 
 const app = express();
 
@@ -18,11 +15,8 @@ app.use(user);
 app.use(scenario);
 app.use(metrics);
 app.use(chart);
-<<<<<<< HEAD
-app.use(cache)
-=======
+app.use(cache);
 app.use(bonzoMl);
->>>>>>> 615949004b892e79b1fef549ed75b5978291279d
 
 /**
  * logic for our api will go here
