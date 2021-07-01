0fdfc4af4f9fdfa13fbfb72d16255ed8c81cc2c8
anilmitra440 <anilm@aretove.com>
2021-07-01 16:03:47 +0530

    Added blocks comments

--- pages/forecast.vue
+++ pages/forecast.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>Forecast page</h1>
+    <!-- Filters component (Vishal) -->
+    <!-- Applied filters pills (Vishal) -->
+    <!-- Base Year/Quarter Stats / Filtered Year/Quarter Stats (Vishal) -->
+    <!-- Base Metrics / Filtered Metrics (Jubin) -->
+    <!-- Filter: Revenue table (Jubin) -->
+    <!-- Filter: Units table (Jubin) -->
+    <!-- Filter: AUR table (Jubin) -->
   </div>
 </template>
 
