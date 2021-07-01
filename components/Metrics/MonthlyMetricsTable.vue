93581e605621dec21cc9609e0e9f5fbeec21c80b
anilmitra440 <anilm@aretove.com>
2021-07-01 23:30:56 +0530

    updated MonthlyMetricsTable

--- components/Metrics/MonthlyMetricsTable.vue
+++ components/Metrics/MonthlyMetricsTable.vue
@@ -11,87 +11,80 @@
             sortable
             label="Metrics Name"
             property="metrics_master.title"
-          ></el-table-column>
+          >
+          </el-table-column>
 
           <el-table-column
             min-width="150"
             sortable
             label="Yearly"
             property="yearly_aggregate"
-          ></el-table-column>
+          >
+            <template slot-scope="scope">{{
+              scope.row.yearly_aggregate | toLocaleStr
+            }}</template>
+          </el-table-column>
 
-          <el-table-column
-            min-width="150"
-            sortable
-            label="Jan"
-            property="jan"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Feb"
-            property="feb"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Mar"
-            property="mar"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Apr"
-            property="apr"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="May"
-            property="may"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Jun"
-            property="jun"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Jul"
-            property="jul"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Aug"
-            property="aug"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Sep"
-            property="sep"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Oct"
-            property="oct"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Nov"
-            property="nov"
-          ></el-table-column
-          ><el-table-column
-            min-width="150"
-            sortable
-            label="Dec"
-            property="dec"
-          ></el-table-column>
+          <el-table-column min-width="150" sortable label="Jan" property="jan">
+            <template slot-scope="scope">{{
+              scope.row.jan | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Feb" property="feb">
+            <template slot-scope="scope">{{
+              scope.row.feb | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Mar" property="mar">
+            <template slot-scope="scope">{{
+              scope.row.mar | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Apr" property="apr">
+            <template slot-scope="scope">{{
+              scope.row.apr | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="May" property="may">
+            <template slot-scope="scope">{{
+              scope.row.may | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Jun" property="jun">
+            <template slot-scope="scope">{{
+              scope.row.jun | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Jul" property="jul">
+            <template slot-scope="scope">{{
+              scope.row.jul | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Aug" property="aug">
+            <template slot-scope="scope">{{
+              scope.row.aug | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Sep" property="sep">
+            <template slot-scope="scope">{{
+              scope.row.sep | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Oct" property="oct">
+            <template slot-scope="scope">{{
+              scope.row.oct | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Nov" property="nov">
+            <template slot-scope="scope">{{
+              scope.row.nov | toLocaleStr
+            }}</template>
+          </el-table-column>
+          <el-table-column min-width="150" sortable label="Dec" property="dec">
+            <template slot-scope="scope">{{
+              scope.row.dec | toLocaleStr
+            }}</template>
+          </el-table-column>
         </el-table>
       </card>
     </div>
