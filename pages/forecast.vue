ad2789c6996132185ab9500f56ecf9dd0ce07e7e
JubinTJ <jubin@tommyjohnwear.com>
2021-07-05 10:36:54 +0530

    Added brandValues Variable

--- pages/forecast.vue
+++ pages/forecast.vue
@@ -303,7 +303,7 @@ export default {
       this.newNessValues = values;
     },
     getBrandValus(values) {
-      this.getBrandValus = values;
+      this.brandValues = values;
     },
     getChannelValues(values) {
       this.channelValues = values;
@@ -386,6 +386,7 @@ export default {
     },
     async showFilteredMetricsByDuration(activeTab) {
       this.filteredActiveTab = activeTab;
+
       let requestedFilterOption = this.emptyFieldCleaner(this.regularFilters);
 
       if (this.filteredActiveTab == "Weekly") {
@@ -531,6 +532,7 @@ export default {
         filter_collections: this.collectionValues,
         filter_skus: this.skuValues,
       };
+      console.log("this.regularFilters", this.regularFilters);
       let requestedFilterOption = this.emptyFieldCleaner(this.regularFilters);
       this.allAppliedFilters = [];
       for (let [key, value] of Object.entries(this.regularFilters)) {
