d7f71b283272e39c1766cee951091f31911207ae
vishaldandwate8411 <vishal.dandwate@aretove.com>
2021-08-05 20:31:47 +0530

    complete implementation for filters

--- api/controller/cache.js
+++ api/controller/cache.js
@@ -1,4 +1,7 @@
 import memcached from "memcached";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const memcachedConnection = new memcached("localhost:11211", {
   retries: 10,
@@ -6,6 +9,18 @@ const memcachedConnection = new memcached("localhost:11211", {
   remove: true,
 });
 
+let globalProgramDataSet = [];
+let globalRegularDataSet = [];
+export const programFiltersData = async (req, res) => {
+  res.json({
+    response: globalProgramDataSet,
+  });
+};
+export const regularFiltersData = async (req, res) => {
+  res.json({
+    response: globalRegularDataSet,
+  });
+};
 export const regularFilterDropdownData = async (req, res) => {
   try {
     memcachedConnection.getMulti(
@@ -103,4 +118,113 @@ export const filterAllOptions = async (req, res) => {
       error: new Error(error),
     });
   }
+};
+
+export const setProgramFilterCache = async (req, res) => {
+  const program = await prisma.$queryRaw(`SELECT
+                                IFNULL(TRIM(dp.product_third_party), 'N/A') AS product_source,
+                                IFNULL(TRIM(dp.product_morphe_new_brand_3p), 'N/A') AS brand_type,
+                                IFNULL(dp.life_cycle, 'N/A') AS life_cycle,
+                                CASE
+                                    WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
+                                    ELSE 'Old'
+                                END AS newness,
+                                IFNULL(dp.brand, 'N/A') AS brand,
+                                IFNULL(dp.ns_product_subcollection, 'N/A') AS collab,
+                                dfbwm.channel AS channel,
+                                dfbwm.country AS sub_channel,
+                                IFNULL(dp.ns_category, 'N/A') AS category,
+                                IFNULL(dp.ns_class, 'N/A') AS class,
+                                IFNULL(dp.ns_subclass, 'N/A') AS sub_class,
+                                IFNULL(dp.ns_collection, 'N/A') AS collection,
+                                CONCAT(dp.SKU, ': ', dp.title) AS sku
+                            FROM
+                                morphe_staging.demand_forecast_base_weekly_metrics dfbwm
+                            JOIN morphe_staging.demand_forecast_run_log dfrl ON
+                                dfrl.id = dfbwm.demand_forecast_run_log_id
+                            JOIN morphe_staging.dim_products dp ON
+                                dp.SKU = dfbwm.sku
+                            WHERE
+                                dfrl.is_base_forecast = true
+                                AND dp.ns_product_group <> 'MORPHE'
+                            GROUP BY
+                                1,
+                                2,
+                                3,
+                                4,
+                                5,
+                                6,
+                                7,
+                                8,
+                                9,
+                                10,
+                                11,
+                                12,
+                                13
+                            ORDER BY
+                                1,
+                                2,
+                                3,
+                                4,
+                                5,
+                                6,
+                                7,
+                                8,
+                                9,
+                                10,
+                                11,
+                                12,
+                                13;`);
+  globalProgramDataSet = program;
+  const regular = await prisma.$queryRaw(`SELECT
+                                              IFNULL(TRIM(dp.product_third_party), 'N/A') AS product_source,
+                                              IFNULL(TRIM(dp.product_morphe_new_brand_3p), 'N/A') AS brand_type,
+                                              IFNULL(dp.life_cycle, 'N/A') AS life_cycle,
+                                              CASE
+                                                WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
+                                                ELSE 'Old'
+                                              END AS newness,
+                                              IFNULL(dp.brand, 'N/A') AS brand,
+                                              dfbwm.channel AS channel,
+                                              dfbwm.country AS sub_channel,
+                                              IFNULL(dp.ns_category, 'N/A') AS category,
+                                              IFNULL(dp.ns_collection, 'N/A') AS collection,
+                                              CONCAT(dp.SKU, ': ', dp.title) AS sku
+                                            FROM
+                                              morphe_staging.demand_forecast_base_weekly_metrics dfbwm
+                                            JOIN morphe_staging.demand_forecast_run_log dfrl ON
+                                              dfrl.id = dfbwm.demand_forecast_run_log_id
+                                            JOIN morphe_staging.dim_products dp ON
+                                              dp.SKU = dfbwm.sku
+                                            WHERE
+                                              dfrl.is_base_forecast = true
+                                            GROUP BY
+                                              dp.product_third_party,
+                                              dp.product_morphe_new_brand_3p,
+                                              dp.life_cycle,
+                                              CASE
+                                                WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
+                                                ELSE 'Old'
+                                              END,
+                                              dp.brand,
+                                              dfbwm.channel,
+                                              dfbwm.country,
+                                              dp.ns_category,
+                                              dp.ns_collection,
+                                              dp.SKU
+                                            ORDER BY
+                                              1,
+                                              2,
+                                              3,
+                                              4,
+                                              5,
+                                              6,
+                                              7,
+                                              8,
+                                              9,
+                                              10;`);
+  globalRegularDataSet = regular;
+  res.json({
+    mess: "set successfully..",
+  });
 };
