c30417e17acd78057b57036cf8e60ff56309bcd7
vishaldandwate8411 <vishal.dandwate@aretove.com>
2021-08-06 22:38:39 +0530

    added changes for cache runner

---
+++ cache-scripts/cacheSetter.py
@@ -0,0 +1,15 @@
+import requests
+
+"""
+production url: https://df.bonzo.ai/api/cache-setter-api
+staging url: https://stg-df.bonzo.ai/api/cache-setter-api
+"""
+def makeAjaxCall():
+    r = requests.get('https://stg-df.bonzo.ai/api/cache-setter-api')
+    if(r.status_code):
+        print("process execution completed successfully")
+    else:
+        print("process running failed....")
+
+if __name__ == "__main__":
+    makeAjaxCall()
