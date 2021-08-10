4bd6b6f57a7bf6e0304a308a5520c9b8748c5e47
vishaldandwate8411 <vishal.dandwate@aretove.com>
2021-08-10 21:28:39 +0530

    added changes for cache

--- api/controller/cache.js
+++ api/controller/cache.js
@@ -1,50 +1,34 @@
 import memcached from "memcached";
 import { PrismaClient } from "@prisma/client";
-import MemcachePlus from 'memcache-plus';
+import MemcachePlus from "memcache-plus";
 
 const prisma = new PrismaClient();
-
+let regularKeysArray = [];
+let programKeysArray = [];
 const memcachedConnection = new memcached("localhost:11211", {
   retries: 10,
   retry: 10000,
   remove: true,
 });
-const newCache = new MemcachePlus()
+const newCache = new MemcachePlus();
 export const programFiltersData = async (req, res) => {
-  const program1 = await newCache.get("globalProgramDataSet1")
-  const program2 = await newCache.get("globalProgramDataSet2")
-  const program3 = await newCache.get("globalProgramDataSet3")
-  const program4 = await newCache.get("globalProgramDataSet4")
+  const data = [];
+  for (let key of programKeysArray) {
+    const sliceArray = await newCache.get(`${key}`);
+    data.push(...sliceArray);
+  }
   res.json({
-    response: [
-      ...program1,
-      ...program2,
-      ...program3,
-      ...program4,
-    ]
+    response: data,
   });
 };
 export const regularFiltersData = async (req, res) => {
-  const regular1 =  await newCache.get("globalRegularDataSet1");
-  const regular2 =  await newCache.get("globalRegularDataSet2");
-  const regular3 =  await newCache.get("globalRegularDataSet3");
-  const regular4 =  await newCache.get("globalRegularDataSet4");
-  const regular5 =  await newCache.get("globalRegularDataSet5");
-  const regular6 =  await newCache.get("globalRegularDataSet6");
-  const regular7 =  await newCache.get("globalRegularDataSet7");
-  const regular8 =  await newCache.get("globalRegularDataSet8");
-
+  const data = [];
+  for (let key of regularKeysArray) {
+    const sliceArray = await newCache.get(`${key}`);
+    data.push(...sliceArray);
+  }
   res.json({
-    response: [
-      ...regular1,
-      ...regular2,
-      ...regular3,
-      ...regular4,
-      ...regular5,
-      ...regular6,
-      ...regular7,
-      ...regular8
-    ]
+    response: data,
   });
 };
 export const regularFilterDropdownData = async (req, res) => {
@@ -147,127 +131,144 @@ export const filterAllOptions = async (req, res) => {
 };
 
 export const setProgramFilterCache = async (req, res) => {
- try {
-  const program = await prisma.$queryRaw(`SELECT
-                                          IFNULL(TRIM(dp.product_third_party), 'N/A') AS product_source,
-                                          IFNULL(TRIM(dp.product_morphe_new_brand_3p), 'N/A') AS brand_type,
-                                          IFNULL(dp.life_cycle, 'N/A') AS life_cycle,
-                                          CASE
-                                              WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
-                                              ELSE 'Old'
-                                          END AS newness,
-                                          IFNULL(dp.brand, 'N/A') AS brand,
-                                          IFNULL(dp.ns_product_subcollection, 'N/A') AS collab,
-                                          dfbwm.channel AS channel,
-                                          dfbwm.country AS sub_channel,
-                                          IFNULL(dp.ns_category, 'N/A') AS category,
-                                          IFNULL(dp.ns_class, 'N/A') AS class,
-                                          IFNULL(dp.ns_subclass, 'N/A') AS sub_class,
-                                          IFNULL(dp.ns_collection, 'N/A') AS collection,
-                                          CONCAT(dp.SKU, ': ', dp.title) AS sku
-                                        FROM
-                                          morphe_staging.demand_forecast_base_weekly_metrics dfbwm
-                                        JOIN morphe_staging.demand_forecast_run_log dfrl ON
-                                          dfrl.id = dfbwm.demand_forecast_run_log_id
-                                        JOIN morphe_staging.dim_products dp ON
-                                          dp.SKU = dfbwm.sku
-                                        WHERE
-                                          dfrl.is_base_forecast = true
-                                          AND dp.ns_product_group <> 'MORPHE'
-                                        GROUP BY
-                                          1,
-                                          2,
-                                          3,
-                                          4,
-                                          5,
-                                          6,
-                                          7,
-                                          8,
-                                          9,
-                                          10,
-                                          11,
-                                          12,
-                                          13
-                                        ORDER BY
-                                          1,
-                                          2,
-                                          3,
-                                          4,
-                                          5,
-                                          6,
-                                          7,
-                                          8,
-                                          9,
-                                          10,
-                                          11,
-                                          12,
-                                          13;`);
-await newCache.set("globalProgramDataSet1", program.slice(0, 1000))
-await newCache.set("globalProgramDataSet2", program.slice(1000, 3000))
-await newCache.set("globalProgramDataSet3", program.slice(3000, 6000))
-await newCache.set("globalProgramDataSet4", program.slice(6000, 9000))
+  try {
+    programKeysArray = [];
+    const program = await prisma.$queryRaw(`SELECT
+IFNULL(TRIM(dp.product_third_party), 'N/A') AS product_source,
+IFNULL(TRIM(dp.product_morphe_new_brand_3p), 'N/A') AS brand_type,
+IFNULL(dp.life_cycle, 'N/A') AS life_cycle,
+CASE
+WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
+ELSE 'Old'
+END AS newness,
+IFNULL(dp.brand, 'N/A') AS brand,
+IFNULL(dp.ns_product_subcollection, 'N/A') AS collab,
+dfbwm.channel AS channel,
+dfbwm.country AS sub_channel,
+IFNULL(dp.ns_category, 'N/A') AS category,
+IFNULL(dp.ns_class, 'N/A') AS class,
+IFNULL(dp.ns_subclass, 'N/A') AS sub_class,
+IFNULL(dp.ns_collection, 'N/A') AS collection,
+CONCAT(dp.SKU, ': ', dp.title) AS sku
+FROM
+morphe_staging.demand_forecast_base_weekly_metrics dfbwm
+JOIN morphe_staging.demand_forecast_run_log dfrl ON
+dfrl.id = dfbwm.demand_forecast_run_log_id
+JOIN morphe_staging.dim_products dp ON
+dp.SKU = dfbwm.sku
+WHERE
+dfrl.is_base_forecast = true
+AND dp.ns_product_group <> 'MORPHE'
+GROUP BY
+1,
+2,
+3,
+4,
+5,
+6,
+7,
+8,
+9,
+10,
+11,
+12,
+13
+ORDER BY
+1,
+2,
+3,
+4,
+5,
+6,
+7,
+8,
+9,
+10,
+11,
+12,
+13;`);
+    let prevIndex = 1;
+    let nextIndex = 3000;
+    let index = 1;
+    for (let i = 1; i <= program.length / 3000; i++) {
+      await newCache.set(
+        `globalProgramDataSet${index}`,
+        program.slice(prevIndex, nextIndex)
+      );
+      programKeysArray.push(`globalProgramDataSet${index}`);
+      prevIndex = nextIndex;
+      nextIndex = prevIndex + nextIndex;
+      index++;
+    }
+    regularKeysArray = [];
+    const regular = await prisma.$queryRaw(`SELECT
+IFNULL(TRIM(dp.product_third_party), 'N/A') AS product_source,
+IFNULL(TRIM(dp.product_morphe_new_brand_3p), 'N/A') AS brand_type,
+IFNULL(dp.life_cycle, 'N/A') AS life_cycle,
+CASE
+WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
+ELSE 'Old'
+END AS newness,
+IFNULL(dp.brand, 'N/A') AS brand,
+dfbwm.channel AS channel,
+dfbwm.country AS sub_channel,
+IFNULL(dp.ns_category, 'N/A') AS category,
+IFNULL(dp.ns_collection, 'N/A') AS collection,
+CONCAT(dp.SKU, ': ', dp.title) AS sku
+FROM
+morphe_staging.demand_forecast_base_weekly_metrics dfbwm
+JOIN morphe_staging.demand_forecast_run_log dfrl ON
+dfrl.id = dfbwm.demand_forecast_run_log_id
+JOIN morphe_staging.dim_products dp ON
+dp.SKU = dfbwm.sku
+WHERE
+dfrl.is_base_forecast = true
+GROUP BY
+dp.product_third_party,
+dp.product_morphe_new_brand_3p,
+dp.life_cycle,
+CASE
+WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
+ELSE 'Old'
+END,
+dp.brand,
+dfbwm.channel,
+dfbwm.country,
+dp.ns_category,
+dp.ns_collection,
+dp.SKU
+ORDER BY
+1,
+2,
+3,
+4,
+5,
+6,
+7,
+8,
+9,
+10;`);
 
-const regular = await prisma.$queryRaw(`SELECT
-                IFNULL(TRIM(dp.product_third_party), 'N/A') AS product_source,
-                IFNULL(TRIM(dp.product_morphe_new_brand_3p), 'N/A') AS brand_type,
-                IFNULL(dp.life_cycle, 'N/A') AS life_cycle,
-                CASE
-                  WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
-                  ELSE 'Old'
-                END AS newness,
-                IFNULL(dp.brand, 'N/A') AS brand,
-                dfbwm.channel AS channel,
-                dfbwm.country AS sub_channel,
-                IFNULL(dp.ns_category, 'N/A') AS category,
-                IFNULL(dp.ns_collection, 'N/A') AS collection,
-                CONCAT(dp.SKU, ': ', dp.title) AS sku
-              FROM
-                morphe_staging.demand_forecast_base_weekly_metrics dfbwm
-              JOIN morphe_staging.demand_forecast_run_log dfrl ON
-                dfrl.id = dfbwm.demand_forecast_run_log_id
-              JOIN morphe_staging.dim_products dp ON
-                dp.SKU = dfbwm.sku
-              WHERE
-                dfrl.is_base_forecast = true
-              GROUP BY
-                dp.product_third_party,
-                dp.product_morphe_new_brand_3p,
-                dp.life_cycle,
-                CASE
-                  WHEN YEAR(dp.launch_date) >= YEAR(CURRENT_DATE()) THEN 'New'
-                  ELSE 'Old'
-                END,
-                dp.brand,
-                dfbwm.channel,
-                dfbwm.country,
-                dp.ns_category,
-                dp.ns_collection,
-                dp.SKU
-              ORDER BY
-                1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                7,
-                8,
-                9,
-                10;`);
-await newCache.set("globalRegularDataSet1", regular.slice(0, 1000))
-await newCache.set("globalRegularDataSet2", regular.slice(1000, 3000))
-await newCache.set("globalRegularDataSet3", regular.slice(3000, 6000))
-await newCache.set("globalRegularDataSet4", regular.slice(6000, 9000))
-await newCache.set("globalRegularDataSet5", regular.slice(9000, 12000))
-await newCache.set("globalRegularDataSet6", regular.slice(12000, 15000))
-await newCache.set("globalRegularDataSet7", regular.slice(15000, 18000))
-await newCache.set("globalRegularDataSet8", regular.slice(18000, 20000))
-res.json({
-mess: "set successfully..",
-});
- }catch(error) {
-  res.json({
-    eror: `${error}`
+    let prevIndex2 = 1;
+    let nextIndex2 = 3000;
+    let index2 = 1;
+    for (let j = 1; j <= program.length / 3000; j++) {
+      await newCache.set(
+        `globalRegularDataSet${index2}`,
+        program.slice(prevIndex2, nextIndex2)
+      );
+      regularKeysArray.push(`globalRegularDataSet${index2}`);
+      prevIndex2 = nextIndex2;
+      nextIndex2 = prevIndex2 + nextIndex2;
+      index2++;
+    }
+
+    res.json({
+      mess: "set successfully..",
     });
- }
+  } catch (error) {
+    res.json({
+      eror: `${error}`,
+    });
+  }
 };
