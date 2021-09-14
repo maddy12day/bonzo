b5dad8b2459f9d2b9a877eabaaac91da5b4fc4c9
Ubuntu <ubuntu@ip-10-0-0-87.us-west-1.compute.internal>
2021-09-14 11:05:59 +0000

    server changes

--- cache-scripts/cacheSetter.py
+++ cache-scripts/cacheSetter.py
@@ -5,7 +5,7 @@ production url: https://df.bonzo.ai/api/cache-setter-api
 staging url: https://stg-df.bonzo.ai/api/cache-setter-api
 """
 def makeAjaxCall():
-    r = requests.get('https://stg-df.bonzo.ai/api/cache-setter-api')
+    r = requests.get('https://df.bonzo.ai/api/cache-setter-api')
     if(r.status_code):
         print("process execution completed successfully")
     else:
