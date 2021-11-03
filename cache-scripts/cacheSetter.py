733a16d6c06fed1c27182dd8b950cf35bcfebc05
vishaldandwate8411 <vishal.dandwate@aretove.com>
2021-11-03 18:08:21 +0530

    added changes to switch between stg to prod for memcached

--- cache-scripts/cacheSetter.py
+++ cache-scripts/cacheSetter.py
@@ -6,8 +6,8 @@ production url: https://df.bonzo.ai/api/cache-setter-api
 staging url: https://stg-df.bonzo.ai/api/cache-setter-api
 """
 def makeAjaxCall():
-    print('https://{serverName}.bonzo.ai/api/cache-setter-api'.format(serverName=sys.argv[0]))
-    r = requests.get('https://{serverName}.bonzo.ai/api/cache-setter-api'.format(serverName=sys.argv[0]))
+    print('https://{serverName}.bonzo.ai/api/cache-setter-api'.format(serverName=sys.argv[1]))
+    r = requests.get('https://{serverName}.bonzo.ai/api/cache-setter-api'.format(serverName=sys.argv[1]))
     if(r.status_code):
         print("process execution completed successfully")
     else:
