a9ce0a1a1c64d8ce8e6ca1a129a400e3ba9792ee
vishaldandwate8411 <vishal.dandwate@aretove.com>
2021-11-11 18:29:01 +0530

    removed console

--- api/controller/forecast.js
+++ api/controller/forecast.js
@@ -808,7 +808,6 @@ const getWeeklyFilteredForecastMetricsQuery = (
   }
                 GROUP BY
                   dfbwm.week_of_year;`;
-                  console.log(query)
   return query;
 };
 
@@ -888,7 +887,6 @@ const getMonthlyFilteredForecastMetricsQuery = (
   }
     GROUP BY
       dfbwm.month_of_year;`;
-      console.log(query)
   return query;
 };
 
@@ -996,7 +994,6 @@ export const getFilteredForecastMetrics = async (req, res) => {
         countryQuery,
         filterForecastedYear
       );
-      console.log("monthlyquery------",query);
     } else {
       query = getWeeklyFilteredForecastMetricsQuery(
         transaction_db,
@@ -1006,8 +1003,6 @@ export const getFilteredForecastMetrics = async (req, res) => {
         countryQuery,
         filterForecastedYear
       );
-      console.log("Weeklyquery------",query);
-
     }
 
     let filteredForecastMetricsPromise = await Promise.allSettled([
@@ -1425,7 +1420,6 @@ export const getFilterChartData = async (req, res) => {
       prisma.$queryRaw(filteredQuarterlyPlannedDataQuery),
       prisma.$queryRaw(filteredQuarterlyThisYearSaleDataQuery),
       prisma.$queryRaw(filteredQuarterlyForecastDataQuery),
-      console.log(filteredQuarterlyForecastDataQuery)
     ]);
     let data = quarterlyFilteredStats.map((item) =>
       parseChartData(req.body.duration, item.value)
